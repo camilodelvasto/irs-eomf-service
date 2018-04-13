b5c60f820d2d061a401f4eb4555d7e0076ddcc95
Camilo Delvasto <camilodelvasto@gmail.com>
2018-04-12 20:44:02 -0500

    Remove unused config vars

--- src/config/vars.js
+++ src/config/vars.js
@@ -10,11 +10,5 @@ module.exports = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   apiKey: process.env.API_KEY,
-  jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES,
-  mongo: {
-    uri: process.env.NODE_ENV === 'test'
-      ? process.env.MONGO_URI_TESTS
-      : process.env.MONGO_URI,
-  },
   logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
 };
