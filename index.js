892488137c7b1f1dfb6616f0e7250a5633da2096
Camilo Delvasto <camilodelvasto@gmail.com>
2018-04-24 14:36:27 -0500

    Use js instead of plain JSON for sequelize config

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = require('./config/express');
 var SwaggerExpress = require('swagger-express-mw');
 
 // listen to requests
-//const server = app.listen(port, () => console.info(`server started on port ${port} (${env})`));
+const server = app.listen(port, () => console.info(`server started on port ${port} (${env})`));
+server.timeout = 1800000;
 
 
 /**
@@ -26,8 +27,8 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   swaggerExpress.register(app);
 
   var port = process.env.PORT || 10010;
-  const server = app.listen(port);
-  server.timeout = 1800000;
+  //const server = app.listen(port);
+  //server.timeout = 1800000;
 
   if (swaggerExpress.runner.swagger.paths['/hello']) {
     console.log('try this:\ncurl http://127.0.0.1:' + port + '/v1/nonprofits');
