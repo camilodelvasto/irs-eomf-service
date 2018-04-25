2ba99514230bcfdeed8273587285d17a43f84213
Camilo Delvasto <camilodelvasto@gmail.com>
2018-04-25 10:44:28 -0500

    Run through Swagger only in dev time

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const { port, env } = require('./config/vars');
 const app = require('./config/express');
 var SwaggerExpress = require('swagger-express-mw');
 
-// listen to requests
-const server = app.listen(port, () => console.info(`server started on port ${port} (${env})`));
-server.timeout = 1800000;
-
-
 /**
 * Exports express
 * @public
@@ -20,17 +15,18 @@ var config = {
   appRoot: __dirname // required config
 };
 
-SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
+// Run through Swagger middleware only in dev env.
+if (process.env.NODE_ENV === 'development') {
+  SwaggerExpress.create(config, function(err, swaggerExpress) {
+    if (err) { throw err; }
 
-  // install middleware
-  swaggerExpress.register(app);
+    // install middleware
+    swaggerExpress.register(app);
 
-  var port = process.env.PORT || 10010;
-  //const server = app.listen(port);
-  //server.timeout = 1800000;
+    var port = process.env.PORT || 10010;
+  });  
+}
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/v1/nonprofits');
-  }
-});
+// Fire up server.
+const server = app.listen(port, () => console.info(`server started on port ${port} (${env})`));
+server.timeout = 1800000;
