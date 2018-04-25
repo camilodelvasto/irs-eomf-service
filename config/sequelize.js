9935b8b75bfadebd9cb8bfa668422ca34c30c90a
Camilo Delvasto <camilodelvasto@gmail.com>
2018-04-24 19:25:30 -0500

    Change configuration so that it reads DB connection from env vars

---
+++ config/sequelize.js
@@ -0,0 +1,15 @@
+const vars = require('./vars.js');
+
+module.exports = {
+  url: process.env.DB_CONNECTION_URL,
+  dialect: process.env.DB_DIALECT,
+  operatorsAliases: 'Op',
+  connectionTimeout: 0,
+  pool: {
+    max: 100,
+    min: 1,
+    idle: 200000,
+    acquire: 200000,
+  },
+  logging: vars.env === 'development' ? false : '',
+};
--- config/vars.js
+++ config/vars.js
@@ -12,5 +12,11 @@ module.exports = {
   apiKey: process.env.API_KEY,
   irsEOMFUrl: process.env.irsEOMFUrl,
   demoDataPort: process.env.demoDataPort,
+  DB_CONNECTION_URL: process.env.DB_CONNECTION_URL,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+  DB_HOST: process.env.DB_HOST,
+  DB_DIALECT: process.env.DB_DIALECT,
   logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
 };
